88cf750dbe1f558e4ad040d737beb92f8eaff123
Kevin Linux <contatokevinlopes@gmail.com>
2026-06-20 00:36:33 -0300

    Alterações que faltaram

--- checkout/backend/src/controllers/checkout.ts
+++ checkout/backend/src/controllers/checkout.ts
@@ -2,5 +2,7 @@ import type { CheckoutRequest } from "../dtos/checkout.dto.ts";
 
 export const checkoutController = async (checkout: CheckoutRequest, reply: any) => {
   
+  
+
   reply.send({ success: true, message: 'Checkout completed successfully' });
 }
--- checkout/backend/src/events/event.ts
+++ checkout/backend/src/events/event.ts
@@ -2,3 +2,6 @@ import { EventEmitter } from 'node:events';
 
 export const myEmitter: EventEmitter | null =
   process.env.CLUSTER !== 'true' ? new EventEmitter() : null;
+
+
+  
